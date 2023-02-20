c3faff2b3fed7cf034800073550017e892adf8fe
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2023-02-20 18:27:33 +0530

    ready 🚀

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const server = express();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5500;
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
@@ -15,6 +15,10 @@ server.get('/nexusmelody/', (req, res) => {
 
 server.get('/notenavigator/', (req, res) => {
   res.redirect('https://nexus-melody.itskdhere.repl.co/');
+});
+
+server.get('/joinserver/', (req, res) => {
+  res.redirect('https://discord.com/invite/9CDh4Qh6vz');
 });
 
 server.listen(port, () => {
